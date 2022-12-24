c2b4a7ed751677f3242a89236fc09a47b19cb4c4
mukul2259 <dhirmukul3@gmail.com>
2022-12-24 00:06:38 +0000

    dubai: Set WITH_GMS to true
     * For obvious reasons, duh

--- aosp_dubai.mk
+++ aosp_dubai.mk
@@ -22,6 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/dubai/device.mk)
 
 # Inherit some common Pixel Experience stuff.
+WITH_GMS := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_dubai
